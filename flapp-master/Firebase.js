481dcafb25f773abdbcdec7d5963bcc4873c638b
Daniel Döbereiner <35856242+Dandober@users.noreply.github.com>
2022-09-21 14:56:46 -0300

    Update Firebase.js

--- flapp-master/Firebase.js
+++ flapp-master/Firebase.js
@@ -1,14 +1,14 @@
 import firebase from 'firebase'
 //Firebase config
 var firebaseConfig = {
-  apiKey: "AIzaSyAyQOcyk4o4o2PJVW-glmyW9gqahmG0hyw",
-  authDomain: "flapp-appdomengao.firebaseapp.com",
-  databaseURL: "https://flapp-appdomengao.firebaseio.com",
-  projectId: "flapp-appdomengao",
-  storageBucket: "flapp-appdomengao.appspot.com",
-  messagingSenderId: "124306057673",
-  appId: "1:124306057673:web:7a0e21fe9154084f472018",
-  measurementId: "G-F5BPLSCGV3"
+  apiKey: 
+  authDomain: 
+  databaseURL: 
+  projectId: 
+  storageBucket: 
+  messagingSenderId: 
+  appId: 
+  measurementId: 
 };
 
  
